045a2620d61e9c3bb8b34c2422ea4a8265174c8f
Bruno Brill Thums <bruno.thums@universo.univates.br>
2023-08-29 10:08:54 -0300

    Added logo

--- hc.py
+++ hc.py
@@ -5,6 +5,36 @@ import subprocess
 from termcolor import colored # Add color to text
 from urllib.parse import urlsplit, urlunsplit # Functions to work with URL
 
+# Function to colorize the response code and reason
+def get_colored_response_status(status_code, status_code_reason):
+    colors = {
+        "1xx": "blue",
+        "2xx": "green",
+        "3xx": "cyan",
+        "4xx": "yellow",
+        "5xx": "red",
+        "other": "white"
+    }
+    
+    if 100 <= status_code < 200:
+        category = "1xx"
+    elif 200 <= status_code < 300:
+        category = "2xx"
+    elif 300 <= status_code < 400:
+        category = "3xx"
+    elif 400 <= status_code < 500:
+        category = "4xx"
+    elif 500 <= status_code < 600:
+        category = "5xx"
+    else:
+        category = "other"
+    
+    color = colors[category]
+    colored_status = colored(str(status_code), color)
+    colored_reason = colored(status_code_reason, color)
+    
+    return f"{colored_status} {colored_reason}"
+
 # Function used to add https:// at the beginning of the URL and remove the subdirectories
 def normalize_url(url, use_ports_80_443=True):
     if not url.startswith("http://") and not url.startswith("https://"):
@@ -28,12 +58,15 @@ def normalize_url(url, use_ports_80_443=True):
         return [url_80, url_443]
     return url
 
-def check_headers(url, show_all_headers=False, show_report=True, debug=False, show_unreachables_URLs=False):
+def check_headers(url, show_all_headers=False, show_report=True, debug=False, show_instant_errors=False):
         # Disable warning for TLS < 1.2
         urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
         divisor = "\n"+("="*40)+"\n"
+        # found_header
         f_h = []
+        # missed_header
         m_h = []
+        # normal_header
         n_h = []
         try:
             # create a sesssion, capable of reusing TCP connections for many HTTP requests
@@ -56,28 +89,26 @@ def check_headers(url, show_all_headers=False, show_report=True, debug=False, sh
             found_header = None
             normal_header = None
             missed_header = None
-            if not show_unreachables_URLs: print(f"\nAnalyzing {url}:\n")
-            response = session.get(url, headers=headers, timeout=10, verify=False)
-            if not show_unreachables_URLs: 
-                status_code = response.status_code
-                status_code_reason = response.reason
-                response_status = str(status_code) + " " + status_code_reason
-                print(colored(response_status, "green"))
+            if show_instant_errors: print("\nAnalyzing", colored(f"{url}", "light_yellow"),": \n")
+            response = session.get(url, headers=headers, timeout=5, verify=False)
+            if show_instant_errors: 
+                colored_response_status = get_colored_response_status(response.status_code, response.reason)
+                print(colored_response_status)
             for header, value in response.headers.items():
                 if show_all_headers:
                     if header in specific_headers:
                         found_header = f"{header}: {value}"
                         f_h.append(found_header)
-                        if not show_unreachables_URLs: print(colored(found_header, "magenta"))
+                        if show_instant_errors: print(colored(found_header, "magenta"))
                     else:
                         normal_header = f"{header}: {value}"
                         n_h.append(normal_header)
-                        if not show_unreachables_URLs: print(colored(normal_header, "white"))
+                        if show_instant_errors: print(colored(normal_header, "white"))
                 else:
                     if header in specific_headers:
                         found_header = f"{header}: {value}"
                         f_h.append(found_header)
-                        if not show_unreachables_URLs: print(colored(found_header, "magenta"))
+                        if show_instant_errors: print(colored(found_header, "magenta"))
             # if
             if show_report:
                 missing_headers = []
@@ -88,14 +119,12 @@ def check_headers(url, show_all_headers=False, show_report=True, debug=False, sh
                     for missing_header in missing_headers:
                         missed_header = f"Missing {missing_header} header"
                         m_h.append(missed_header)
-                        if not show_unreachables_URLs: print(colored(missed_header, "magenta"))
-            # print exclusively for show_unreachables_URLs
-            if (found_header or missed_header) and show_unreachables_URLs:
-                print(f"Analyzing {url}:\n")
-                status_code = response.status_code
-                status_code_reason = response.reason
-                response_status = str(status_code) + " " + status_code_reason
-                print(colored(response_status, "green"))
+                        if show_instant_errors: print(colored(missed_header, "magenta"))
+            # print exclusively for show_instant_errors
+            if (found_header or missed_header) and not show_instant_errors:
+                print("\nAnalyzing", colored(f"{url}", "light_yellow"),": \n")
+                colored_response_status = get_colored_response_status(response.status_code, response.reason)
+                print(colored_response_status)
                 if n_h and show_all_headers:
                     for header in n_h: print(colored(header, "white"))
                 if f_h:
@@ -103,7 +132,7 @@ def check_headers(url, show_all_headers=False, show_report=True, debug=False, sh
                 if m_h:
                     for header in m_h: print(colored(header, "magenta"))
                 print(divisor)
-            if not show_unreachables_URLs: print(divisor)
+            if show_instant_errors: print(divisor)
         
         except KeyboardInterrupt:
             print("\nScript interrupted by user.")
@@ -114,7 +143,7 @@ def check_headers(url, show_all_headers=False, show_report=True, debug=False, sh
             if debug:
                 print(colored(f"SSL Error: {ssl_error}", "red"))
                 print(divisor)
-            elif show_unreachables_URLs:
+            elif not show_instant_errors:
                 pass
             else:
                 print(colored(f"SSL Error", "red"))
@@ -124,7 +153,7 @@ def check_headers(url, show_all_headers=False, show_report=True, debug=False, sh
             if debug:
                 print(colored(f"Read Timeout: {read_timeout}", "red"))
                 print(divisor)
-            elif show_unreachables_URLs:
+            elif not show_instant_errors:
                 pass
             else:
                 print(colored(f"Read Timeout", "red"))
@@ -134,20 +163,24 @@ def check_headers(url, show_all_headers=False, show_report=True, debug=False, sh
             if debug:
                 print(colored(f"Connection Error: {connection_error}", "red"))
                 print(divisor)
-            elif show_unreachables_URLs:
+            elif not show_instant_errors:
                 pass
             else:
                 print(colored(f"Connection Error", "red"))
                 print(divisor)
             url_connection_error.append(url)
-    
-        
-    
+def show_logo():                                                                                                      
+    print("    __                   __               __              __    ")
+    print("   / /_  ___  ____ _____/ /__  __________/ /_  ___  _____/ /__  ")
+    print("  / __ \/ _ \/ __ `/ __  / _ \/ ___/ ___/ __ \/ _ \/ ___/ //_/  ")
+    print(" / / / /  __/ /_/ / /_/ /  __/ /  / /__/ / / /  __/ /__/ ,<     ")
+    print("/_/ /_/\___/\__,_/\__,_/\___/_/   \___/_/ /_/\___/\___/_/|_|    ")
 
 if __name__ == "__main__":
     urls_with_timeout = []  # Initialize the list to store URLs with timeout
     url_sslerror = []
     url_connection_error = []
+
     # Define some request headers to bypass (a little) tools/systems/solutions anti-bot
     if len(sys.argv) < 2:
         print("Usage: python script.py <url_or_file>")
@@ -155,22 +188,30 @@ if __name__ == "__main__":
         print("-> show all response headers")
         print(colored("--no-ports -np ", "green", attrs=['bold']),end="")
         print("-> don't add ports 80 and 443. Go all https (443)")
-        print(colored("--no-report -nr ", "green", attrs=['bold']),end="")
+        print(colored("--no-csp-hsts -nr ", "green", attrs=['bold']),end="red")
         print("-> don't verify CSP or HTST")
         print(colored("--debug -d ", "green", attrs=['bold']),end="")
         print("-> show detailed errors")
-        print(colored("--no-unreachable -nu ", "green", attrs=['bold']),end="")
-        print("-> don't show at the end the urls with connection errors. Shows at the instant")
+        print(colored("--show-instant-errors -si ", "green", attrs=['bold']),end="")
+        print("-> don't show, at the end, the urls with connection errors. Shows at the instant")
+        print("\n", colored("All parameters are", "light_grey"), colored("false", "light_red"), colored("by default", "light_grey"))
         sys.exit(1)
     
+    # Initialization definitions
     target = sys.argv[1]
+    # False by default
     show_all_headers = "--all-headers" in sys.argv or "-h" in sys.argv
+    # True by default
     use_ports_80_443 = not ("--no-ports" in sys.argv or "-np" in sys.argv)
-    show_report = not ("--no-report" in sys.argv or "-nr" in sys.argv)
+    # True by default
+    show_report = not ("--no-csp-hsts" in sys.argv or "-nc" in sys.argv)
+    # False by default
     debug = "--debug" in sys.argv or "-d" in sys.argv
-    show_unreachables_URLs = not ("--no-unreachable" in sys.argv or "-nu" in sys.argv)
+    # True by default
+    show_instant_errors = "--show-instant-errors" in sys.argv or "-si" in sys.argv
     
     specific_headers = ["Server", "Via", "X-Powered-By", "X-AspNet-Version", "Host"]
+    show_logo()
     
     try:
         # read the content of the file
@@ -198,12 +239,12 @@ if __name__ == "__main__":
             "Connection": "keep-alive"
         }
     for target_url in target_urls:
-        check_headers(target_url, show_all_headers, show_report, debug, show_unreachables_URLs)
+        check_headers(target_url, show_all_headers, show_report, debug, show_instant_errors)
         #print("="*40)  # Separate output for each URL
     #if urls_with_timeout:
         #for target_url in urls_with_timeout:
             #check_headers_curl(target_url, show_all_headers, show_report)
-    if (show_unreachables_URLs):
+    if not show_instant_errors:
         if urls_with_timeout:
             print(colored("URLs with timeout:", "red"))
             for url in urls_with_timeout:
